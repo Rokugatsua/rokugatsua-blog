dcf2e8b29586040cb17e77ece9057974650611a7
rokugatsua <rokugatsua@outlook.co.id>
2023-08-28 21:55:55 +0700

    add hero image with sharp plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost = ({data, children}) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>{data.mdx.frontmatter.date}</p>
+            <GatsbyImage image={image} />
             {children}
         </Layout>
     )
@@ -18,6 +21,11 @@ export const query = graphql `
             frontmatter {
                 title
                 date(formatString: "MMMM D, YYYY")
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
         }
     }

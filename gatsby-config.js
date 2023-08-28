1682ac1da91b2254b25a875ffa9a6fcf0cd617fe
rokugatsua <rokugatsua@outlook.co.id>
2023-08-28 17:25:59 +0700

    rm adapter

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,13 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-const adapter = require("gatsby-adapter-netlify")
 
 module.exports = {
-  adapter: adapter({
-    excludeDatastoreFromEngineFunction: false,
-  }),
   siteMetadata: {
     title: `rokugatsua-blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    "gatsby-plugin-netlify",
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`
+    "gatsby-plugin-netlify"
   ],
 }

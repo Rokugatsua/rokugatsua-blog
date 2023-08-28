a7fd5893914f89f2e46b1b51e853813a96402324
rokugatsua <rokugatsua@outlook.co.id>
2023-08-28 20:44:53 +0700

    add cloudinary and image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-
+require('dotenv').config();
 module.exports = {
   siteMetadata: {
     title: "rokugatsua-blog",
@@ -10,6 +10,9 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-netlify",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -17,6 +20,29 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
-    "gatsby-plugin-mdx"
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
+      }
+    },
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: `image`,
+        prefix: `gatsby-source-cloudinary/`,
+      }
+    },
+    {
+      resolve: `gatsby-transformer-cloudinary`,
+      options: {
+        uploadFolder: `gatsby-cloudinary`,
+        transformTypes: [`CloudinaryMedia`],
+      }
+    },
   ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,14 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 const AboutPage = () => {
     return (
         <Layout pageTitle={"About Me"}>
             <p>this page it's about me.</p>
+            <StaticImage src="https://yt3.ggpht.com/ytc/AMLnZu_WBKt0fYv-RMkoGFJWgX2-wzZrbRbhEivY8Tv7Ow=s800-c-k-c0x00ffffff-no-rj" />
         </Layout>
     )
 }

8de1c1fed1c6deb301fc9aa7e994744fb564b6a9
rokugatsua <rokugatsua@outlook.co.id>
2023-08-29 12:45:10 +0700

    add content to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,17 @@ import Seo from "../components/seo"
 const IndexPage = () => {
   return(
     <Layout pageTitle={"Home Page"}>
-      <p>this a rokugatsua blog</p>
+      <div className="row">
+        <div className="column">
+          <h1>Hello, I am Juni</h1>
+          <h3>I am self taught programmer and I make a game.</h3>
+          <div>
+            <a href="/about" className="button button-clear">About me</a>
+            <a href="/about" className="button button-clear">My works</a>
+          </div>
+        </div>
+        <div className="column"></div>
+      </div>
     </Layout>
   )
 }
